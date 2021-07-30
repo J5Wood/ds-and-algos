ab5b9c751f32391b6a7aacca1565d7fe286b6bf2
Jeremy Wood <js.wood206@gmail.com>
2021-07-30 00:36:30 -0700

    Completed

---
+++ insertionSort.js
@@ -0,0 +1,14 @@
+function insertionSort(arr){
+    for (let i =1; i < arr.length; i++){
+        let currentVal = arr[i];
+
+        for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--){
+            console.log(i,j, currentVal)
+            arr[j + 1] = arr[j]
+        }
+        arr[j + 1] = currentVal
+    }
+    return arr;
+}
+
+console.log(insertionSort([2,23,54,34,2,6,345,4756,4,345,24,65,8]))
