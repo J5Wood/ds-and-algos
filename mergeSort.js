14d01a0c4d07e11b0947f01aea9b3b9e35e7957f
Jeremy Wood <js.wood206@gmail.com>
2021-07-31 22:14:15 -0700

    Completed

--- mergeSort.js
+++ mergeSort.js
@@ -1,10 +1,13 @@
-function mergeSort(arr) {
+function myMergeSort(arr) {
 
+    if (arr.length <= 1){
+        return arr;
+    }
 
-    //split array into pieces 0 - 1 long
-    
+    let arr2 = myMergeSort(arr.splice(0, Math.floor(arr.length / 2)))
+    let arr1 =  myMergeSort(arr)
 
-
+    return sortArrays(arr1, arr2)
 
     function sortArrays(arrOne, arrTwo){
         let returnArr = [];
@@ -22,8 +25,50 @@ function mergeSort(arr) {
         }
         return returnArr;
     }
-
-    console.log(sortArrays(arr.splice(arr.length / 2), arr))
 }
 
-console.log(mergeSort([1,23,24,56,100,21,23,47,72,82,99]))
+
+
+
+function mergeSort(arr) {
+
+    if (arr.length <= 1){
+        return arr;
+    }
+
+    let mid = Math.floor(arr.length / 2)
+    let arr1 = mergeSort(arr.slice(0, mid))
+    let arr2 = mergeSort(arr.slice(mid))
+
+    return sortArrays(arr1, arr2)
+
+    function sortArrays(arrOne, arrTwo){
+        let returnArr = [];
+        indexOne= 0;
+        indexTwo= 0;
+
+        while (indexOne < arrOne.length && indexTwo < arrTwo.length){
+            if(arrTwo[indexTwo] > arrOne[indexOne]){
+                returnArr.push(arrOne[indexOne]);
+                indexOne++;
+            } else {
+                returnArr.push(arrTwo[indexTwo]);
+                indexTwo++;
+            }
+        }
+        while (indexOne < arrOne.length){
+            returnArr.push(arrOne[indexOne])
+            indexOne++;
+        }
+        while (indexTwo < arrTwo.length){
+            returnArr.push(arrTwo[indexTwo])
+            indexTwo++;
+        }
+        return returnArr;
+    }
+}
+
+let arr = [1,23,24,56,100,21,23,47,72,82,99,45,234,245,54,45,345,34,534,534,655,465,56,567,657,657,5675,75,74,63,63,563576,575,7]
+
+
+console.log(mergeSort(arr), myMergeSort(arr))
