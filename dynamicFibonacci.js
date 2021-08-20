b8a27c7a50ef38a0b9a447438b2abdb062b3f40f
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2021-08-19 23:01:22 -0700

    Altered solution

--- dynamicFibonacci.js
+++ dynamicFibonacci.js
@@ -8,9 +8,8 @@
 // Smart fibonacci
 // Storing data in a "memo" like an array or an obj allows us to access those results 
 //  instead of having to do more calculations that repeat work we've already completed
-function fib(n, memo = []){
+function fib(n, memo = [undefined, 1, 1]){
     if(memo[n] !== undefined) return memo[n];
-    if(n <= 2) return 1;
     let value = fib(n-1, memo) + fib(n-2, memo);
     memo[n] = value
     return value;
